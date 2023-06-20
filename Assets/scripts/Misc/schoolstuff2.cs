83c070225660230ab0bed86b9d03c5e4b1c790b5
ddogg231 <116034321+ddogg231@users.noreply.github.com>
2023-06-19 22:43:34 -0400

    stuff

---
+++ Assets/scripts/Misc/schoolstuff2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class schoolstuff2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // if (isGrounded && inputManager.playerIsmoving)
+      // {
+      //     PlayFootstepSound();
+      //
+      //
+      //     curMoveInput = inputManager.moveDir * moveSpeed * Time.deltaTime;
+      //     transform.forward = inputManager.moveDir;
+      //
+      //     rb.MovePosition(rb.position + curMoveInput);
+      //     if (isGrounded && inputManager.playerIsmoving)
+      //     {
+      //         PlayFootstepSound();
+      //         walkingEffect.Play();
+      //
+      //         Stashed changes
+      //     }
+      //     else
+      //     {
+      //         StopFootstepSound();
+      //         Updated upstream
+      //     }
+      //
+      //
+      //     walkingEffect.Stop();
+      // }
+      // Stashed changes
+    }
+}
